9d33d196148f6089e190ebb3462d2090ea1d02d4
zikriyachaudary <91419529+zikriyachaudary@users.noreply.github.com>
2023-01-03 11:41:41 +0500

    Add files via upload

---
+++ selection ascending.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int size,a,loc,temp, min;
+	cout << "Enter the Size of an Array ==>  ";
+	cin >> size;
+	int arr[size];
+	cout << "Enter "<< size <<" values :  ";
+	for(int j=0; j<size; j++)
+	{
+		cin >> arr[j];
+	}
+	for(int i=0; i<size-1; i++)
+	{
+		min = arr[i];
+		loc = i;
+		for(int zz=0; zz<(3*size)/2; zz++)
+		{
+			cout << "__";
+		}
+			cout << endl;
+		for(int k=i+1; k<size; k++)
+		{
+			if(arr[k]<min)
+			{
+				min = arr[k];
+				loc = k;
+			}
+		}
+		for(int j=0; j<size; j++)
+		{
+			cout << arr[j] << " |";
+		}
+		cout << endl;
+		temp = arr[i];
+		arr[i] = arr[loc];
+		arr[loc] = temp;
+		for(int zz=0; zz<(3*size)/2; zz++)
+		{
+			cout << "--";
+		}
+		cout << endl;
+	}
+	return 0;
+}
